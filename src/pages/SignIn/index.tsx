9580d273e1d45b6143d57393cc8fd109e7fc662b
rubensojunior <rubensojunior6@gmail.com>
2020-04-26 12:08:42 -0300

    stylin signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,13 +3,28 @@ import { Image } from 'react-native'
 
 import { Container, Title } from './styles'
 
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
 import logo from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logo} />
+
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('teste')
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   )
 }
