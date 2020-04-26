721a8c1908e49b16e29183906bec28c5d8ae6794
rubensojunior <rubensojunior6@gmail.com>
2020-04-26 13:36:03 -0300

    adding navigation between signin and signup pages

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import {
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
 
-import { Container } from './styles'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import logo from '../../assets/logo.png'
+
+import { Container, Title, BackToSign, BackToSignText } from './styles'
 
 const SignUp: React.FC = () => {
-  return <Container />
+  const navigation = useNavigation()
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logo} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('teste')
+              }}
+            >
+              Cadastrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSign
+        onPress={() => {
+          navigation.navigate('SignIn')
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignText>Voltar para logon</BackToSignText>
+      </BackToSign>
+    </>
+  )
 }
 
 export default SignUp
