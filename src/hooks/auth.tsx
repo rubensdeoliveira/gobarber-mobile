285275c6b64e6045d85e8ee6b143bb58111e8853
rubensojunior <rubensojunior6@gmail.com>
2020-06-14 17:11:49 -0300

    implementing dashboard header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,16 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage'
 import api from '../services/api'
 
+interface User {
+  id: string
+  name: string
+  email: string
+  avatar_url: string
+}
+
 interface AuthState {
   token: string
-  user: Object
+  user: User
 }
 
 interface SingInCredentials {
@@ -19,7 +26,7 @@ interface SingInCredentials {
 }
 
 interface AuthContextData {
-  user: Object
+  user: User
   loading: boolean
   signIn(credentials: SingInCredentials): Promise<void>
   signOut(): void
@@ -39,6 +46,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ])
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`
+
         setData({ token: token[1], user: JSON.parse(user[1]) })
       }
 
@@ -60,6 +69,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ])
+
+    api.defaults.headers.authorization = `Bearer ${token}`
 
     setData({ token, user })
   }, [])
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,52 @@
-import React from 'react'
-import { View, Button } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import api from '../../services/api'
 import { useAuth } from '../../hooks/auth'
 
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  Username,
+  ProfileButton,
+  UserAvatar,
+} from './styles'
+
+interface Provider {
+  id: string
+  name: string
+  avatar_url: string
+}
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth()
+  const [providers, setProviders] = useState<Provider[]>([])
+
+  const { signOut, user } = useAuth()
+  const { navigate } = useNavigation()
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data)
+    })
+  }, [])
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile')
+  }, [navigate])
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <Username>{user.name}</Username>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
   )
 }
 
